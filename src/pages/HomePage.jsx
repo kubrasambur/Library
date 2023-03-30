7d34f00b62ffe25378fb9c602fb58972c9451412
kubrasambur <kubrasambur123@gmail.com>
2023-03-30 14:51:19 +0300

    async storage and deleting book

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,15 @@
-import React, { useState } from "react";
-import { Text, HStack, ScrollView, VStack, Button } from "native-base";
+import React, { useEffect, useState } from "react";
+import { Text, HStack, ScrollView, VStack, Button, Center } from "native-base";
 import SearchBar from "../components/SearchBar";
 import CustomAddBookModal from "../components/custom/CustomAddBookModal";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function HomePage() {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
-  const [filteredData, setFilteredData] = useState([
-    {
-      id: 1,
-      title: "The Lord of the Rings",
-    },
-  ]);
+  const [filteredData, setFilteredData] = useState([]);
   const [search, setSearch] = useState("");
-  const [masterDataSource, setMasterDataSource] = useState([
-    {
-      id: 1,
-      title: "The Lord of the Rings",
-    },
-  ]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
 
   function handleAddBook() {
     setFilteredData((prevBooks) => [
@@ -37,7 +28,27 @@ export default function HomePage() {
     ]);
     setValue("");
     setOpen(false);
+    AsyncStorage.setItem(
+      "books",
+      JSON.stringify([
+        ...filteredData,
+        {
+          id: filteredData.length + 1,
+          title: value,
+        },
+      ])
+    );
+    AsyncStorage.getItem("books").then((value) => {
+      console.log("value1", value);
+      setFilteredData(JSON.parse(value));
+    });
   }
+  useEffect(() => {
+    AsyncStorage.getItem("books").then((value) => {
+      console.log("value2", value);
+      setFilteredData(JSON.parse(value));
+    });
+  }, []);
 
   function handleFilter(text) {
     if (text) {
@@ -54,6 +65,18 @@ export default function HomePage() {
       setFilteredData(masterDataSource);
       setSearch(text);
     }
+  }
+
+  function handleEdit() {
+    console.log("edit");
+  }
+
+  function handleDelete(id) {
+    AsyncStorage.getItem("books").then((books) => {
+      const alteredBooks = JSON.parse(books).filter((book) => book.id !== id);
+      AsyncStorage.setItem("books", JSON.stringify(alteredBooks));
+      setFilteredData(alteredBooks);
+    });
   }
 
   return (
@@ -73,11 +96,22 @@ export default function HomePage() {
             w="92%"
             display="flex"
             justifyContent="space-between"
+            alignItems="center"
           >
-            <Text px="3%" py="2%">
+            <Text px="3%" py="2%" w="61%">
               {book.title}
             </Text>
-            <Button w="22%">delete</Button>
+            <Center
+              flexDirection="row"
+              w="39%"
+              display="flex"
+              justifyContent="center"
+            >
+              <Button onPress={handleEdit}>Edit</Button>
+              <Button ml={3} onPress={() => handleDelete(book.id)}>
+                delete
+              </Button>
+            </Center>
           </HStack>
         ))}
       </ScrollView>
